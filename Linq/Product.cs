c1685cc3de6950738e1fdac025f3bec584feb6f2
Akshay Khanna <akshay9911102163@gmail.com>
2018-02-12 00:07:38 +0530

    Added code

---
+++ Linq/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public int QuantityAvailable { get; set; }
+        static int count = 1000;
+        public Product()
+        {
+            this.ProductId = (count++);
+        }
+    }
+}
